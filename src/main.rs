ef4e26cef1a61ba9084a103fe9c150dbdde452fc
Wahid Niam <iam.wahidn@gmail.com>
2024-04-18 22:08:03 +0700

    user agent

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::{io::{BufRead, Read, Write}, net::TcpListener};
+use std::{collections::HashMap, io::{BufRead, Read, Write}, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -23,12 +23,21 @@ fn main() {
                     .collect_tuple()
                     .unwrap();
 
+                let mut headers: HashMap<&str, &str> = HashMap::new();
+                for header in http_request[1..].into_iter() {
+                    let (header_name, header_value) = header.split(":").map(|s| s.trim()).take(2).collect_tuple().unwrap();
+                    headers.entry(header_name).or_insert(header_value);
+                }
+
                 let (status_line, content) = match path {
                     "/" => ("HTTP/1.1 200 OK", "OK"),
                     a => {
                         if a.starts_with("/echo/") {
                             let word = a.trim_start_matches("/echo/");
                             ("HTTP/1.1 200 OK", word)
+                        } else if a.starts_with("/user-agent") {
+                            let user_agent = *headers.get("User-Agent").unwrap();
+                            ("HTTP/1.1 200 OK", user_agent)
                         } else {
                             ("HTTP/1.1 404 Not Found", "Not Found")
                         }
