dc90285ea53d9365030b35bd42cbd97edaef6c43
Wahid Niam <iam.wahidn@gmail.com>
2024-04-18 21:26:18 +0700

    return 404

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::Write, io::Read, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -6,7 +6,17 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                let mut buffer = [0; 1024];
+                stream.read(&mut buffer).unwrap();
+
+                let index = b"GET / HTTP/1.1\r\n";
+                let status_line = if buffer.starts_with(index) {
+                    "HTTP/1.1 200 OK"
+                } else {
+                    "HTTP/1.1 404 Not Found"
+                };
+                
+                let response = format!("{}\r\n\r\n", status_line);
                 if stream.write_all(response.as_bytes()).is_err() {
                     println!("Error writing to stream");
                 }
