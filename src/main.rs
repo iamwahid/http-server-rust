190b23268c713e138b5abf0138ae9caf8c545742
Wahid Niam <iam.wahidn@gmail.com>
2024-04-18 21:13:08 +0700

    return 200

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                if stream.write_all(response.as_bytes()).is_err() {
+                    println!("Error writing to stream");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
